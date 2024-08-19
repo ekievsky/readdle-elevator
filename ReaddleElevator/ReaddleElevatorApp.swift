ea4d4692a391e3f119e2d1c50b65a6377fbb206e
Evgenii Kievsky <evgenii.kievsky@gmail.com>
2024-08-19 12:06:42 +0200

    Initial Commit

---
+++ ReaddleElevator/ReaddleElevatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaddleElevatorApp.swift
+//  ReaddleElevator
+//
+//  Created by Yevhen Kyivskyi on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReaddleElevatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
