64b35f15ef768e628c38bcc332b0e40006b0272a
Evgenii Kievsky <evgenii.kievsky@gmail.com>
2024-08-19 13:32:06 +0200

    Removed commented code

--- ReaddleElevator/Features/Elevator/ElevatorViewModel.swift
+++ ReaddleElevator/Features/Elevator/ElevatorViewModel.swift
@@ -15,32 +15,7 @@ import SwiftUI
     
     private var enqueuedFloors: [Int] = []
     private var isAnimating = false
-    
-//    func seeelectFLoor(_ newFloor: Int) {
-//        let item = selectItem(newFloor)
-//        
-//        item.notify(queue: .main, execute: <#T##DispatchWorkItem#>)
-//        
-////        let item = DispatchWorkItem {
-////            withAnimation(.linear(duration: 1)) { [weak self] in
-////                self?.selectedFloor = newFloor
-////            }
-////        }
-////        
-////        DispatchQueue.main.async(execute: item)
-////        
-////        item.notify(queue: .main, execute: )
-//        
-//    }
-//    
-//    func selectItem(_ newFloor: Int) -> DispatchWorkItem {
-//        DispatchWorkItem {
-//            withAnimation(.linear(duration: 1)) { [weak self] in
-//                self?.selectedFloor = newFloor
-//            }
-//        }
-//    }
-//    
+
     func selectFloor(_ newFloor: Int) {
         enqueueFloor(newFloor)
     }
